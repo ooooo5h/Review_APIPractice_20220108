83b2a2744ceaec2ed9e2900d6c68458e7e58cb54
전은형 <ooooo5h91@gmail.com>
2022-01-08 08:51:16 +0900

    Initial commit

---
+++ app/src/main/java/com/eunhyung/review_apipractice_20220108/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.eunhyung.review_apipractice_20220108
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
