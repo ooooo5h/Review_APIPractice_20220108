b164074c31583801b2b8c9bc3e972821ef65a5f9
전은형 <ooooo5h91@gmail.com>
2022-01-08 09:34:10 +0900

    서버의 응답을 추가 분석   - {} => BasicResponse   - data : {} => DataResponse   - user : {} => UserData

---
+++ app/src/main/java/com/eunhyung/review_apipractice_20220108/models/DataResponse.kt
@@ -0,0 +1,7 @@
+package com.eunhyung.review_apipractice_20220108.models
+
+class DataResponse(
+    val user : UserData,
+    val token : String,
+) {
+}
---
+++ app/src/main/java/com/eunhyung/review_apipractice_20220108/models/UserData.kt
@@ -0,0 +1,8 @@
+package com.eunhyung.review_apipractice_20220108.models
+
+class UserData(
+    val id : Int,
+    val nick_name : String,
+    val profile_img : String,
+) {
+}
