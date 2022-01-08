f0950e9c9d5c04967c298e2c08a9a9dd3e91e443
전은형 <ooooo5h91@gmail.com>
2022-01-08 11:46:33 +0900

    닉네임과 프사 실제 파싱해서 반영(이미 파싱되어있는 구조 재활용함)

--- app/src/main/java/com/eunhyung/review_apipractice_20220108/MyProfileActivity.kt
+++ app/src/main/java/com/eunhyung/review_apipractice_20220108/MyProfileActivity.kt
@@ -2,8 +2,10 @@ package com.eunhyung.review_apipractice_20220108
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
 import com.eunhyung.review_apipractice_20220108.models.BasicResponse
 import com.eunhyung.review_apipractice_20220108.utils.ContextUtil
+import kotlinx.android.synthetic.main.activity_my_profile.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -20,6 +22,18 @@ class MyProfileActivity : BaseActivity() {
         apiList.getRequestMyInfo(ContextUtil.getToken(mContext)).enqueue(object : Callback<BasicResponse>{
             override fun onResponse(call: Call<BasicResponse>, response: Response<BasicResponse>) {
 
+                if (response.isSuccessful) {
+
+                    val br = response.body()!!
+
+//                    사용자가 누구인지 변수에 담자
+                    val myInfo = br.data.user
+
+//                    UI에 닉네임/프사 등 반영
+                    txtMyNickname.text = myInfo.nick_name
+                    Glide.with(mContext).load(myInfo.profile_img).into(imgMyProfile)
+
+                }
             }
 
             override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
