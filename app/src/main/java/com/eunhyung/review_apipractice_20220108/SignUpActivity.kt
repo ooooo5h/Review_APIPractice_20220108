7995ccaaa2a4c2b499663c140f37e3198b2f7605
전은형 <ooooo5h91@gmail.com>
2022-01-08 09:47:30 +0900

    회원가입기능 2)회원가입 버튼 이벤트 : 레트로핏/기능 목록 -> 회원가입 기능이 없다

--- app/src/main/java/com/eunhyung/review_apipractice_20220108/SignUpActivity.kt
+++ app/src/main/java/com/eunhyung/review_apipractice_20220108/SignUpActivity.kt
@@ -2,6 +2,8 @@ package com.eunhyung.review_apipractice_20220108
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.eunhyung.review_apipractice_20220108.api.APIList
+import com.eunhyung.review_apipractice_20220108.api.ServerAPI
 import kotlinx.android.synthetic.main.activity_sign_up.*
 
 class SignUpActivity : AppCompatActivity() {
@@ -15,6 +17,10 @@ class SignUpActivity : AppCompatActivity() {
             val inputPw = edtPassword.text.toString()
             val inputNickname = edtNickname.text.toString()
             val inputPhoneNum = edtPhone.text.toString()
+
+//            API 호출(회원가입) => ServerAPI + APIList 조합
+            val retrofit = ServerAPI.getRetrofit()
+            val apiList = retrofit.create(APIList::class.java)
         }
     }
 }
