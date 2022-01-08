0022f625fa03b238f0bade8141d07416fc45772f
전은형 <ooooo5h91@gmail.com>
2022-01-08 09:04:06 +0900

    레트로핏 객체 생성 완료   - 이제 ServerAPI.getRetrofit()로 사용 가능함

--- app/src/main/java/com/eunhyung/review_apipractice_20220108/api/ServerAPI.kt
+++ app/src/main/java/com/eunhyung/review_apipractice_20220108/api/ServerAPI.kt
@@ -1,6 +1,7 @@
 package com.eunhyung.review_apipractice_20220108.api
 
 import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 
 class ServerAPI {
 
@@ -15,6 +16,19 @@ class ServerAPI {
 
 //        retrofit 변수에 환경설정 + 객체화해서 다른 곳에서는 쉽게 가져다 쓸 수 있게 함수를 만들자
         fun getRetrofit() : Retrofit {
+            
+//            retrofit변수가 null이라면 새 객체를 만들어주고, 그게 아니라면 있는 객체를 사용하자
+//            하나의 객체를 계속 재활용하게 유도하는 디자인 패턴을 싱글톤 패턴이라고 함
+            if (retrofit == null){
+                
+                retrofit = Retrofit.Builder()
+                    .baseUrl(BASE_URL)  // 어느 서버안에서 움직일거니?
+                    .addConverterFactory(GsonConverterFactory.create())  // 서버가 내려주는 응답은 JSON인데, 자동으로 분석해주는 도구 설치
+                    .build()
+            }
+    
+//             return대는 retrofit은 절대 null일 리 없다고 명시
+            return retrofit!!
 
         }
 
