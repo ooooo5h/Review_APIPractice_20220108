d80631d6b833aef8f0c7488e40810691c3576a1d
전은형 <ooooo5h91@gmail.com>
2022-01-08 10:34:31 +0900

    BaseActivity에서 물려줄 변수로 apiList변수 추가   - onCreate함수안에서, 세팅까지 모두 마친채로 물려주도록 세팅

--- app/src/main/java/com/eunhyung/review_apipractice_20220108/BaseActivity.kt
+++ app/src/main/java/com/eunhyung/review_apipractice_20220108/BaseActivity.kt
@@ -1,6 +1,25 @@
 package com.eunhyung.review_apipractice_20220108
 
+import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.eunhyung.review_apipractice_20220108.api.APIList
+import com.eunhyung.review_apipractice_20220108.api.ServerAPI
+import retrofit2.Retrofit
 
 abstract class BaseActivity : AppCompatActivity() {
+
+    lateinit var retrofit: Retrofit
+    lateinit var apiList: APIList
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+//       이 내부의 코드는 모든 화면들의 시작지점에서 공통적으로 실행되는 코드
+
+//        레트로핏 관련 설정들을 여기서 진행함(모든 화면들이 자동 실행)
+
+        retrofit = ServerAPI.getRetrofit()
+        apiList = retrofit.create(APIList::class.java)
+
+    }
 }
