50bd545719c31619e5454537a6f025bf495ea9bf
전은형 <ooooo5h91@gmail.com>
2022-01-08 09:57:10 +0900

    회원가입기능 5)API 응답 분석 UI 처리

--- app/src/main/java/com/eunhyung/review_apipractice_20220108/SignUpActivity.kt
+++ app/src/main/java/com/eunhyung/review_apipractice_20220108/SignUpActivity.kt
@@ -2,9 +2,14 @@ package com.eunhyung.review_apipractice_20220108
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.eunhyung.review_apipractice_20220108.api.APIList
 import com.eunhyung.review_apipractice_20220108.api.ServerAPI
+import com.eunhyung.review_apipractice_20220108.models.BasicResponse
 import kotlinx.android.synthetic.main.activity_sign_up.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class SignUpActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,6 +28,36 @@ class SignUpActivity : AppCompatActivity() {
             val apiList = retrofit.create(APIList::class.java)
 
             apiList.putRequestSignUp(inputEmail, inputPw, inputNickname, inputPhoneNum)
+                .enqueue(object : Callback<BasicResponse>{
+                override fun onResponse(
+                    call: Call<BasicResponse>,
+                    response: Response<BasicResponse>
+                ) {
+
+//                    회원가입 요청 => DB에 가입 => 서버가 내려준 응답이 돌아왔을 때 실행하는 부분
+//                    response.isSuccessful => 코드값이 200인가?
+                    if (response.isSuccessful){
+
+//                        BasicResponse 형태로 자동 분석된 응답은 200으로 돌아왔을때만 정상 동작함
+                        val br = response.body()!!  // BasicResponse를 추출
+
+//                         회원가입 성공처리만 br변수를 이용해서 진행
+
+                    }
+                    else {
+
+//                        문제 발생 ! => BasicResponse 정상 응답 제공 X
+                        val testStr = response.errorBody()!!.string() // 에러의 경우는, 별도로 에러 본문을 확인
+                        Log.d("문제응답", testStr)
+
+                    }
+
+                }
+                override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+
+                }
+
+            })
         }
     }
 }
