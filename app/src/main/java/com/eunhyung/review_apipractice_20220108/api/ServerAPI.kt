54fa5a0d2ae8fb3505bcd7ec264f24fa82efc2e0
전은형 <ooooo5h91@gmail.com>
2022-01-08 09:00:04 +0900

    레트로핏 객체 생성 준비중   - 어느 서버에 연결할지 주소를 담고   - 객체를 담게 될 변수 생성   - 객체를 받아올 떄 사용할 함수 생성중

--- app/src/main/java/com/eunhyung/review_apipractice_20220108/api/ServerAPI.kt
+++ app/src/main/java/com/eunhyung/review_apipractice_20220108/api/ServerAPI.kt
@@ -1,11 +1,23 @@
 package com.eunhyung.review_apipractice_20220108.api
 
+import retrofit2.Retrofit
+
 class ServerAPI {
 
 //    서버 통신을 하게 도와주는 클래스(Retrofit)를 객체로 만들어주는 기능
 //    함수 한번만 호출하면 어디서든 편하게 가져다 쓸 수 있게 함수로 만들기위해서
 //    클래스이름.기능()  => 파이썬의 @classmethod에 대응되는 기능을 활용할꺼임
     companion object {
+
+//        이 클래스 내부에서만 사용할 예정 => private 처리
+        private val BASE_URL = "https://api.gudoc.in"
+        private var retrofit : Retrofit? = null  // 서버 연결을 전담하는 변수인데, 기본적으로 안만든 상태(null)처리
+
+//        retrofit 변수에 환경설정 + 객체화해서 다른 곳에서는 쉽게 가져다 쓸 수 있게 함수를 만들자
+        fun getRetrofit() : Retrofit {
+
+        }
+
 
 
     }
