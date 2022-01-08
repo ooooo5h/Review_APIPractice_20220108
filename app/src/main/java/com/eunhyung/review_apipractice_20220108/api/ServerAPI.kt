2643eb63decd7bd75f775f591581000a9a7363b7
전은형 <ooooo5h91@gmail.com>
2022-01-08 11:53:05 +0900

    API 요청을 가로채서 해줄 일 작성

--- app/src/main/java/com/eunhyung/review_apipractice_20220108/api/ServerAPI.kt
+++ app/src/main/java/com/eunhyung/review_apipractice_20220108/api/ServerAPI.kt
@@ -1,5 +1,8 @@
 package com.eunhyung.review_apipractice_20220108.api
 
+import android.content.Context
+import com.eunhyung.review_apipractice_20220108.utils.ContextUtil
+import okhttp3.Interceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
@@ -15,7 +18,7 @@ class ServerAPI {
         private var retrofit : Retrofit? = null  // 서버 연결을 전담하는 변수인데, 기본적으로 안만든 상태(null)처리
 
 //        retrofit 변수에 환경설정 + 객체화해서 다른 곳에서는 쉽게 가져다 쓸 수 있게 함수를 만들자
-        fun getRetrofit() : Retrofit {
+        fun getRetrofit(context: Context) : Retrofit {
             
 //            retrofit변수가 null이라면 새 객체를 만들어주고, 그게 아니라면 있는 객체를 사용하자
 //            하나의 객체를 계속 재활용하게 유도하는 디자인 패턴을 싱글톤 패턴이라고 함
@@ -23,6 +26,24 @@ class ServerAPI {
 
 //                토큰의 경우, 여러 API 함수에서 사용해야하고 매번 같은 토큰값이 입력된다(ContextUtil.getToken())
 //                  =>  자동화하면 편하겠찌? => 레트로핏 객체 생성전에, 토큰에 관련된 세팅을 직접 해둠으로써!
+
+
+//                API 요청이 만들어 질 때 => 가로채서 헤더를 추가해주자
+//                  헤더가 붙여지고 나서, 나머지 api요청을 실행하면 자동으로 헤더첨부효과가 발생함
+                val interceptor = Interceptor {
+                    with(it) {
+
+//                        새 리퀘스트를 만들자(헤더가 첨부된 리퀘스트)
+
+                        val newRequest = request().newBuilder()
+                            .addHeader("X-Http-Token", ContextUtil.getToken(context))
+                            .build()
+
+//                        완성된 새 리퀘스트로 작업을 이어가게 처리
+                        proceed(newRequest)
+                    }
+
+                }
 
                 
                 retrofit = Retrofit.Builder()
