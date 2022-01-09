0937f56a6dac7692815dcc7f8f3ca47693f4fbfc
전은형 <ooooo5h91@gmail.com>
2022-01-09 18:51:24 +0900

    실제 리뷰 등록하는 API 호출까지 완료

--- app/src/main/java/com/eunhyung/review_apipractice_20220108/EditReviewActivity.kt
+++ app/src/main/java/com/eunhyung/review_apipractice_20220108/EditReviewActivity.kt
@@ -2,9 +2,14 @@ package com.eunhyung.review_apipractice_20220108
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.eunhyung.review_apipractice_20220108.models.BasicResponse
 import com.eunhyung.review_apipractice_20220108.models.ProductData
 import kotlinx.android.synthetic.main.activity_edit_review.*
 import kotlinx.android.synthetic.main.activity_main.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class EditReviewActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,6 +27,28 @@ class EditReviewActivity : BaseActivity() {
 
 
 //            서버에 전달하자(POST/review)
+            apiList.postRequestSubmitReview(product.id, inputTitle, inputContent, inputScore, "")
+                .enqueue(object : Callback<BasicResponse>{
+                    override fun onResponse(
+                        call: Call<BasicResponse>,
+                        response: Response<BasicResponse>
+                    ) {
+                        if (response.isSuccessful){
+                            Toast.makeText(mContext, "리뷰 등록 성공", Toast.LENGTH_SHORT).show()
+                            finish()
+                        }
+
+
+                    }
+
+                    override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+
+
+
+                    }
+
+
+                })
 
 
         }
