14f1f0fa94ffab7ed4b2a205d6d5c07db79365d2
전은형 <ooooo5h91@gmail.com>
2022-01-09 18:44:23 +0900

    리뷰 등록 이벤트 처리중

--- app/src/main/java/com/eunhyung/review_apipractice_20220108/EditReviewActivity.kt
+++ app/src/main/java/com/eunhyung/review_apipractice_20220108/EditReviewActivity.kt
@@ -2,10 +2,29 @@ package com.eunhyung.review_apipractice_20220108
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.eunhyung.review_apipractice_20220108.models.ProductData
+import kotlinx.android.synthetic.main.activity_edit_review.*
+import kotlinx.android.synthetic.main.activity_main.*
 
 class EditReviewActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_review)
+
+//        이전 화면에서 넘겨준 데이터 받아오기
+        val product = intent.getSerializableExtra("product") as ProductData
+
+        btnSubmitReview.setOnClickListener {
+
+            val inputTitle = edtTitle.text.toString()
+            val inputContent = edtContent.text.toString()
+            val inputScore = ratingBar.rating
+
+
+//            서버에 전달하자(POST/review)
+
+
+        }
+
     }
 }
