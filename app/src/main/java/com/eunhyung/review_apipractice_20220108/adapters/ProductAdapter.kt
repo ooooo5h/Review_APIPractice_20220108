93e7938d7d90713283e9d880352f44783d24cf29
전은형 <ooooo5h91@gmail.com>
2022-01-09 18:09:52 +0900

    상품 목록을 뿌려주는 어댑터 추가

---
+++ app/src/main/java/com/eunhyung/review_apipractice_20220108/adapters/ProductAdapter.kt
@@ -0,0 +1,44 @@
+package com.eunhyung.review_apipractice_20220108.adapters
+
+import android.content.Context
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.eunhyung.review_apipractice_20220108.R
+import com.eunhyung.review_apipractice_20220108.models.ProductData
+import com.eunhyung.review_apipractice_20220108.models.ReviewData
+
+class ProductAdapter(
+    val mContext : Context,
+    val mList : List<ProductData>) : RecyclerView.Adapter<ProductAdapter.MyViewHolder>() {
+
+    inner class MyViewHolder(val row : View) : RecyclerView.ViewHolder(row){
+
+
+        fun bind(data: ProductData) {
+
+
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+
+        val row = LayoutInflater.from(mContext).inflate(R.layout.product_list_item, parent, false)
+        return MyViewHolder(row)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+
+        holder.bind(mList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return mList.size
+    }
+}
