9397c645c1a6024a571afdcdcd88c6e89cfc9d01
전은형 <ooooo5h91@gmail.com>
2022-01-09 21:01:09 +0900

    리뷰 상세보기 데이터 반영 마무리

--- app/src/main/java/com/eunhyung/review_apipractice_20220108/ViewReviewActivity.kt
+++ app/src/main/java/com/eunhyung/review_apipractice_20220108/ViewReviewActivity.kt
@@ -2,8 +2,10 @@ package com.eunhyung.review_apipractice_20220108
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
 import com.eunhyung.review_apipractice_20220108.models.BasicResponse
 import com.eunhyung.review_apipractice_20220108.models.ReviewData
+import kotlinx.android.synthetic.main.activity_edit_review.*
 import kotlinx.android.synthetic.main.activity_view_reivew.*
 import kotlinx.android.synthetic.main.review_list_item.*
 import kotlinx.android.synthetic.main.review_list_item.txtReviewTitle
@@ -24,8 +26,15 @@ class ViewReviewActivity : BaseActivity() {
                 if (response.isSuccessful){
                     val br = response.body()!!
                     val newReviewData = br.data.review
+
                     txtReviewTitle.text = newReviewData.title
                     txtReviewContent.text = newReviewData.content
+
+                    Glide.with(mContext).load(newReviewData.thumbnail_img).into(imgReviewThumbnail)
+                    Glide.with(mContext).load(newReviewData.user.profile_img).into(imgWriterProfile)
+                    txtWriterNickname.text = newReviewData.user.nick_name
+                    scoreRatingBar.rating = newReviewData.score
+
                 }
 
             }
